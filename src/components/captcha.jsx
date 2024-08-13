340e3b8dd7dbe0d3ffdae4671e8b483b06ecb48b
kencanillo <kenny@shifl.com>
2024-08-13 12:46:53 +0800

    Working na

--- src/components/captcha.jsx
+++ src/components/captcha.jsx
@@ -1,18 +1,21 @@
 import { useState, useEffect, useRef } from 'react';
-import './App.css';
+import '../App.css';
 
-function App() {
-	const [mouseMovement, setMouseMovement] = useState(0);
-    const [shapeToSelect, setShapeToSelect] = useState('');
-    const [colorToSelect, setColorToSelect] = useState('');
+function Captcha({handleValidated})	 {
+	// const [mouseMovement, setMouseMovement] = useState(0);
     const [isScreenshotTaken, setIsScreenshotTaken] = useState(false);
     const videoRef = useRef(null);
     const randomBoxRef = useRef(null);
     const screenshotCanvasRef = useRef(null);
-    const shapeSelectorRef = useRef(null);
 	const [pointsLocation, setPointsLocation] = useState([])
+	const [clickedPoints, setClickedPoints] = useState([])
+	const [xClickedPoints, setXClickedPoints] = useState([])
+	const [attempts, setAttempts] = useState(0);
+	const [maxAttempts, setMaxAttempts] = useState(3); // Maximum allowed attempts
+	const [isValidated, setIsValidated] = useState(null)
 
 	useEffect(() => {
+		// Allow camera to start video stream for screenshot use
 		const video = videoRef.current;
         const randomBox = randomBoxRef.current;
 
@@ -27,11 +30,27 @@ function App() {
         } else {
             console.error('Camera error');
         }
-
+		
 		positionRandomBox(randomBox);
+
+		const canvas = document.getElementById('screenshotCanvas');
+
+        // // Function to handle mouse movement
+        // function handleMouseMove(event) {
+        //     // Get the mouse position relative to the canvas
+        //     const rect = canvas.getBoundingClientRect();
+        //     const x = event.clientX - rect.left;
+        //     const y = event.clientY - rect.top;
+
+		// 	setMouseMovement({x, y})
+        // }
+
+        // // Attach the event listener to the canvas
+        // canvas.addEventListener('mousemove', handleMouseMove);
 	}, [])
 
 	useEffect(() => {
+		// Stop Camera if the screenshot is taken
 		if(isScreenshotTaken) {
 			const video = videoRef.current
 			if (video && video.srcObject) {
@@ -55,8 +74,8 @@ function App() {
 	}, [isScreenshotTaken])
 
 	useEffect(() => {
-		console.log(pointsLocation)
-	}, [pointsLocation])
+		handleValidated(isValidated, maxAttempts, attempts)
+	}, [attempts])
 
 	const addShapes = () => {
         const randomBox = document.getElementById('randomBox');
@@ -128,9 +147,7 @@ function App() {
 		const videoWidth = video.videoWidth;
 		const videoHeight = video.videoHeight;
 	
-		const randomBoxRect = randomBox.getBoundingClientRect();
-		const randomBoxWidth = randomBoxRect.width;
-		const randomBoxHeight = randomBoxRect.height;
+		const videoRect = video.getBoundingClientRect();
 	
 		// Set canvas size to match video size
 		screenshotCanvas.width = videoWidth;
@@ -148,14 +165,14 @@ function App() {
 							  shape.classList.contains('circle') ? 'circle' :
 							  shape.classList.contains('square') ? 'square' : null;
 	
-			// Get position relative to randomBox
+			// Get x and y axis relative to randomBox position within the video frame
 			const shapeRect = shape.getBoundingClientRect();
-			const offsetX = (shapeRect.x) - (randomBoxRect.x);
-			const offsetY = (shapeRect.y) - (randomBoxRect.y);
+			const offsetX = (shapeRect.x) - (videoRect.x);
+			const offsetY = (shapeRect.y) - (videoRect.y);
 	
-			// Convert to canvas coordinates relative to the video frame
-			const x = (offsetX / videoWidth) * videoWidth + videoWidth / 4;
-			const y = (offsetY / videoHeight) * videoHeight + videoHeight / 4;
+			// Convert to canvas coordinates and add a gap relative to the video frame
+			const x = offsetX + 20
+			const y = offsetY + 20
 	
 			let size, color;
 			if (shapeType === 'triangle') {
@@ -186,10 +203,8 @@ function App() {
 			const currentColor = document.getElementById('color').innerText;
 			const currentColorRGB = getColorInRGB(currentColor);
 			const currentShape = document.getElementById('shape').innerText
-			// Check if the current shape matches the selected shape type and color
 
-			console.log('')
-			console.log(color, shapeType)
+			// Check if the current shape matches the selected shape type and color
 			console.log(currentColor.toLowerCase(), currentShape.toLowerCase())
 			if (shapeType === currentShape.toLowerCase() && color === currentColorRGB) {
 				console.log(x, y);
@@ -201,6 +216,8 @@ function App() {
 	};
 
 	const drawTriangle = (ctx, x, y, size, color) => {
+		ctx.save(); // Save the current context state
+		ctx.globalAlpha = 0.5; // Set opacity to 50%
 		ctx.beginPath();
 		ctx.moveTo(x, y - size);
 		ctx.lineTo(x - size, y + size);
@@ -208,18 +225,79 @@ function App() {
 		ctx.closePath();
 		ctx.fillStyle = color;
 		ctx.fill();
+		ctx.restore(); // Restore the context state
 	};
 	
 	const drawCircle = (ctx, x, y, radius, color) => {
+		ctx.save(); // Save the current context state
+		ctx.globalAlpha = 0.5; // Set opacity to 50%
 		ctx.beginPath();
 		ctx.arc(x, y, radius, 0, 2 * Math.PI);
 		ctx.fillStyle = color;
 		ctx.fill();
+		ctx.restore(); // Restore the context state
 	};
 	
 	const drawSquare = (ctx, x, y, size, color) => {
+		ctx.save(); // Save the current context state
+		ctx.globalAlpha = 0.5; // Set opacity to 50%
 		ctx.fillStyle = color;
 		ctx.fillRect(x - size / 2, y - size / 2, size, size);
+		ctx.restore(); // Restore the context state
+	};
+
+	const handleCanvasClick = (event) => {
+		const canvas = screenshotCanvasRef.current;
+		const rect = canvas.getBoundingClientRect();
+		
+		// Calculate the x and y coordinates of the click relative to the canvas
+		const x = event.clientX - rect.left;
+		const y = event.clientY - rect.top;
+		const clickedPoint = { x, y };
+
+		console.log('Clicked position:', clickedPoint);
+
+		// Check if the clicked point is within any shape in pointsLocation
+		const shapeClicked = pointsLocation.find(shape => {
+			if (shape.type === 'square') {
+				return isPointInSquare(clickedPoint, shape);
+			} 
+			else if (shape.type === 'circle') {
+				return isPointInCircle(clickedPoint, shape);
+			} 
+			else if (shape.type === 'triangle') {
+				return isPointInTriangle(clickedPoint, shape);
+			}
+			return false;
+		});
+
+		const ctx = canvas.getContext('2d');
+
+		// Draw a circle at the mouse position
+		ctx.beginPath();
+		ctx.arc(x, y, 10, 0, 2 * Math.PI);
+		ctx.fillStyle = 'orange';
+		ctx.fill();
+		
+		if (shapeClicked) {
+			let  temp = [...clickedPoints]
+			const alreadySelected = clickedPoints.find(shape => {
+				if (shape.x === shapeClicked.x && shape.y === shapeClicked.y) {
+					return true
+				} 
+				return false;
+			});
+
+			if(!alreadySelected) {
+				temp.push(shapeClicked)
+				setClickedPoints(temp)
+			}
+		} else {
+			let temp = [...xClickedPoints]
+
+			temp.push(clickedPoint)
+			setXClickedPoints(temp)
+		}
 	};
 
 	const isPointInSquare = (point, shape) => {
@@ -241,55 +319,68 @@ function App() {
 	
 	const isPointInTriangle = (point, shape) => {
 		const { x, y, size } = shape;
-		const areaOrig = Math.abs((x - size) * (y + size) - (x + size) * (y + size));
-		const area1 = Math.abs((point.x - x) * (point.y - y));
-		const area2 = Math.abs((x - size) * (point.y - y));
-		const area3 = Math.abs((point.x - x) * (y + size) - (x + size) * (point.y - y));
-		return (area1 + area2 + area3) === areaOrig;
+	
+		// Calculate the vertices of the equilateral triangle
+		const height = Math.sqrt(3) / 2 * size;
+		const x1 = x - size / 2;
+		const y1 = y + height / 2;
+		const x2 = x + size / 2;
+		const y2 = y + height / 2;
+		const x3 = x;
+		const y3 = y - height / 2;
+	
+		// Calculate the area of the triangle
+		const areaOrig = Math.abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2);
+		
+		let area1 = 0
+		let area2 = 0
+		let area3 = 0
+		// Calculate the areas of the triangles formed by the point and each pair of vertices
+		area1 = Math.abs((point.x * (y2 - y3) + x2 * (y3 - point.y) + x3 * (point.y - y2)) / 2);
+		area2 = Math.abs((x1 * (point.y - y3) + point.x * (y3 - y1) + x3 * (y1 - point.y)) / 2);
+		area3 = Math.abs((x1 * (y2 - point.y) + x2 * (point.y - y1) + point.x * (y1 - y2)) / 2);
+		
+		console.log((area1 + area2 + area3).toFixed(0), areaOrig)
+		// Check if the point is inside the triangle
+		return (area1 + area2 + area3).toFixed(0) === areaOrig.toFixed(0);
 	};
 
     const validateCaptcha = () => {
-        const selectedShapes = document.querySelectorAll(`.shape.selected.${window.targetShape}`);
-        const allTargetShapes = document.querySelectorAll(`.${window.targetShape}`);
-        const result = document.getElementById('result');
-
-        // Filter shapes by the target color
-		const selectedShapesFiltered = Array.from(selectedShapes).filter(shape => {
-			// Check if the shape is a triangle or not
-			if (shape.classList.contains('triangle')) {
-				// For triangles, color is in the borderBottom style property
-				return shape.style.borderBottom === colorToSelect;
-			} else {
-				// For other shapes, color is in the backgroundColor style property
-				return shape.style.backgroundColor === colorToSelect;
+		const result = document.getElementById('result');
+		const blockUntilCookie = getCookie('blockUntil');
+		let blockUntil = blockUntilCookie ? new Date(blockUntilCookie) : null;
+		let attemp = attempts
+	
+		// Check if the user is currently blocked
+		if (blockUntil && new Date() < blockUntil) {
+			result.textContent = `You are blocked! Try again after ${new Date(blockUntil).toLocaleTimeString()}.`;
+			result.style.color = 'orange';
+			return;
+		}
+	
+		if (clickedPoints.length > 0 && clickedPoints.length === pointsLocation.length && xClickedPoints.length <= 0) {
+			result.textContent = 'CAPTCHA passed!';
+			result.style.color = 'green';
+			deleteCookie('blockUntil'); // Reset block status on success
+			setAttempts(0);
+			setIsValidated(1)
+		} else {
+			attemp++;
+			result.textContent = `CAPTCHA failed! Attempt ${attemp}/${maxAttempts}. Try again.`;
+			result.style.color = 'red';
+	
+			if (attemp >= maxAttempts) {
+				blockUntil = new Date(new Date().getTime() + 5 * 60 * 1000); // Block for 5 minutes
+				setCookie('blockUntil', blockUntil.toUTCString(), 5);
+				result.textContent = `CAPTCHA failed! You are blocked for 5 minutes.`;
+				result.style.color = 'red';
 			}
-		});
+			setIsValidated(0)
 
-		const allTargetShapesFiltered = Array.from(allTargetShapes).filter(shape => {
-			// Check if the shape is a triangle or not
-			if (shape.classList.contains('triangle')) {
-				// For triangles, color is in the borderBottom style property
-				return shape.style.borderBottom === colorToSelect;
-			} else {
-				// For other shapes, color is in the backgroundColor style property
-				return shape.style.backgroundColor === colorToSelect;
-			}
-		});
-
-        if (
-            selectedShapesFiltered.length === allTargetShapesFiltered.length &&
-            selectedShapesFiltered.length > 0
-        ) {
-            result.textContent = 'CAPTCHA passed!';
-            result.style.color = 'green';
-        } else if (selectedShapesFiltered.length !== allTargetShapesFiltered.length) {
-            if (selectedShapesFiltered.length > 0) {
-                result.textContent = 'CAPTCHA failed! Try again.';
-                result.style.color = 'red';
-                addShapes();
-            }
-        }
-    };
+		}
+		
+		setAttempts(attemp)
+	};
 
 	const handleContinue = () => {
 		setPointsLocation([])
@@ -314,7 +405,13 @@ function App() {
         }
     };
 
-	const handleSkip = () => {
+	const handleRetry = () => {
+		// Clear results
+		const result = document.getElementById('result');
+		result.textContent = '';
+		result.style.color = '';
+
+		// Allow camera and clear random box
 		const video = videoRef.current
 		const randomBox = randomBoxRef.current
 		randomBox.innerHTML = ""
@@ -326,60 +423,51 @@ function App() {
 				console.error('Error accessing the camera:', error);
 			});
 
-		setIsScreenshotTaken(false);
+		// Clear states
+		setClickedPoints([])
+		setXClickedPoints([])
+		setPointsLocation([])
+		setIsScreenshotTaken(false)
+		setIsValidated(null)
     };
 
-	const handleBoxClick = () => {
-		console.log(randomBoxRef)
-	}
+	// This is for blocking the user for 5 minutes before they can try again
+	const setCookie = (name, value, minutes) => {
+		const date = new Date();
+		date.setTime(date.getTime() + (minutes * 60 * 1000));
+		const expires = "expires=" + date.toUTCString();
+		document.cookie = name + "=" + value + ";" + expires + ";path=/";
+	};
 
-	const handleCanvasClick = (event) => {
-		const canvas = screenshotCanvasRef.current;
-		const rect = canvas.getBoundingClientRect();
-
-		// Calculate the x and y coordinates of the click relative to the canvas
-		const x = event.clientX - rect.left;
-		const y = event.clientY - rect.top;
-		const clickedPoint = { x, y };
-
-		console.log('Clicked position:', clickedPoint);
-
-		// Check if the clicked point is within any shape in pointsLocation
-		const shapeClicked = pointsLocation.find(shape => {
-			if (shape.type === 'square') {
-				console.log('1 >>>', shape)
-				return isPointInSquare(clickedPoint, shape);
-			} 
-			else if (shape.type === 'circle') {
-				console.log('2 >>>', shape)
-				return isPointInCircle(clickedPoint, shape);
-			} 
-			else if (shape.type === 'triangle') {
-				console.log('3 >>>', shape)
-				return isPointInTriangle(clickedPoint, shape);
+	const getCookie = (name) => {
+		const cookieName = name + "=";
+		const decodedCookie = decodeURIComponent(document.cookie);
+		const cookieArray = decodedCookie.split(';');
+		for (let i = 0; i < cookieArray.length; i++) {
+			let cookie = cookieArray[i].trim();
+			if (cookie.indexOf(cookieName) === 0) {
+				return cookie.substring(cookieName.length, cookie.length);
 			}
-			return false;
-		});
-
-		if (shapeClicked) {
-			console.log('Clicked inside a shape:', shapeClicked);
-			// Do something, e.g., mark the shape as selected
-		} else {
-			console.log('Clicked outside of all shapes.');
 		}
+		return "";
+	};
+
+	const deleteCookie = (name) => {
+		document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 	};
 
     return (
         <>	
-			<div ref={shapeSelectorRef} id="shapeSelector" style={{ 'display': isScreenshotTaken ? 'unset' : 'none' }}>
-                <label id='instruction'></label> <span id="color"></span> <span id="shape"></span>
+			<div id="shapeSelector">
+                <span id='instruction'>Take a Selfie</span>&nbsp;<span id="color"></span>&nbsp;<span id="shape"></span>
             </div>
 
 			{/* Camera Container for the user to see the position of the box where the shapes should be selected */}
             <div className="camera-container">
 				{/* Random Shapes Container */}
 				<canvas ref={screenshotCanvasRef} onClick={handleCanvasClick} id="screenshotCanvas" className='screenshot' style={{ 'display': isScreenshotTaken ? 'unset' : 'none' }}></canvas>
-				<div className="grid" ref={randomBoxRef} id="randomBox" onClick={handleBoxClick} style={{ display: isScreenshotTaken ? 'none' : ''}}></div>
+				<div className="grid" ref={randomBoxRef} id="randomBox" style={{ display: isScreenshotTaken ? 'none' : ''}}></div>
+				{/* <div className="grid" ref={randomBoxRef} id="randomBox"></div> */}
 				
 				{/* Camera stream */}
                 <video ref={videoRef} autoPlay playsInline style={{ 'display': !isScreenshotTaken ? 'unset' : 'none' }}></video>
@@ -388,8 +476,14 @@ function App() {
 			{
 				isScreenshotTaken
 				?	<div className='gap-2'>
-						<button onClick={validateCaptcha} id="continueButton">Validate</button> &nbsp;
-						<button onClick={handleSkip} id="continueButton">Skip</button>
+						{
+							isValidated === null && pointsLocation.length > 1
+							? <button disabled={clickedPoints.length <= 0 && xClickedPoints.length <= 0 } onClick={validateCaptcha} id="continueButton">Validate</button>
+							: 	<>
+									<button disabled={clickedPoints.length <= 0 && xClickedPoints.length <= 0 } onClick={validateCaptcha} id="continueButton">Validate</button>&nbsp;
+									<button onClick={() => { handleRetry() }} id="continueButton">{ pointsLocation.length > 1 ? "Retry" : "Skip" }</button>
+								</>
+						}
 					</div>
 				:	<button onClick={handleContinue} id="continueButton">Continue</button>
 			}
@@ -399,4 +493,4 @@ function App() {
     );
 }
 
-export default App;
+export default Captcha;
---
+++ src/components/results.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect, useRef } from 'react';
+import '../App.css';
+
+function Result({result, maxAttempts, attempts, handleRetry})	 {
+    const passedTexts = [
+        "CAPTCHA: 0, You: 1. Take that, robots!",
+        "You’ve outsmarted the bots... for now.",
+        "Congratulations! You’re officially smarter than a machine.",
+        "CAPTCHA passed! Robots can’t stop you!",
+        "Nice job! You’ve proven your humanity. Now go forth and surf the web.",
+        "You passed the test! Time to celebrate with some internet browsing.",
+        "Congrats! You can now enter the secret lair... or just continue browsing.",
+        "You did it! The robots are jealous of your skills.",
+        "Victory is yours! CAPTCHA couldn’t hold you back.",
+        "You’ve unlocked the next level: The Internet."
+    ];
+
+    const failedTexts = [
+        "Beep boop. Looks like you're one of us now!",
+        "Nice try, human... or are you?",
+        "Robots: 1, You: 0. Try again, fleshbag!",
+        "CAPTCHA failed. The robots are laughing.",
+        "You almost had it, but the robots outsmarted you.",
+        "Bzzzt! Wrong answer. Are you sure you’re not a robot?",
+        "Looks like the machines won this round!",
+        "Access denied. Even robots have standards!",
+        "Computers: 1, Humans: 0. Better luck next time!",
+        "Robot uprising averted... for now."
+    ];
+
+    const [text, setText] = useState('')
+
+    useEffect(() => {
+        console.log(result)
+        getRandomText()
+    }, [])
+
+    const getRandomText = () => {
+        const randomPassedIndex = Math.floor(Math.random() * passedTexts.length);
+        const randomFailedIndex = Math.floor(Math.random() * failedTexts.length);
+        setText(result ? passedTexts[randomPassedIndex] : failedTexts[randomFailedIndex]);
+    }
+
+    return (
+        <>	
+			{
+                result
+                ?   <>
+                        <div className='passed-group'>
+                            <img className='passed-img' src="https://media.giphy.com/media/GkoPRrSUXKpuw2Ql7I/giphy.gif?cid=790b76116w4a28q0e0zbweu7f5m47yy5rraq1sco2lwh8dct&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="" width={100} height={100}/>
+                            <span className='passed-text'>{ text }</span>
+                        </div>
+                    </>
+                :   <>
+                        <div className='failed-group'>
+                            <img className='failed-img' src="https://media.giphy.com/media/ro2tOPw8ywqJi/giphy.gif?cid=ecf05e47enzdfpqrrrdeeygqrj8hmtopdyyuu7k9dhy4qa3j&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="" width={200} height={150}/>
+                            <span className='failed-text'>{ text }</span>
+                        </div>
+                        <div className='mb-3'>
+                            <button type='button' id="continueButton" onClick={handleRetry}>Try Again</button>
+                        </div>
+                        <div className='mb-3'>
+                            <span style={{color: 'red'}}>{`CAPTCHA failed! Attempt ${attempts}/${maxAttempts}. Try again.`}</span>
+                        </div>
+                    </>
+            }
+        </>
+    );
+}
+
+export default Result;
